44b3f652ce12afc504e3494a783b25afa140a92e
Marlon Leon <ghostveene@gmail.com>
2023-02-09 12:58:19 -0500

    fetching colorsarray

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const baseUrl = "https://www.thecolorapi.com/scheme"
 const colorPicker = document.getElementById("choose-color")
 const colorMode = document.getElementById("color-scheme-mode")
 const getColorsBtn = document.getElementById("get-color-scheme-btn")
+const mainElement = document.getElementById("colors-holder")
 let scheme
 
 
@@ -9,17 +10,33 @@ let scheme
 const randomColor = Math.floor(Math.random() * 16777215)
 colorPicker.value = "#" + randomColor.toString(16)
 
-/* Display mode on select drop down */
+/* Display mode on select drop down and retrieving colors */
 const getColors = () => {
     fetch(`${baseUrl}?hex=${colorPicker.value.slice(1)}&mode=${colorMode.value}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data.colors)
+            renderColor(data.colors)
+        })
 }
 
-const addColorSchemeMode = () => {
-    Object.entries(scheme).forEach(
+getColors()
+// const addColorSchemeMode = () => {
+//     Object.entries(scheme).forEach(
 
-    )
+//     )
+// }
+
+// getColorsBtn.addEventListener("click", () => getColors())
+
+
+/* render colors */
+
+const renderColor = (colorsArray) => {
+    mainElement.innerHTML += colorsArray.map(colors => {
+        return `
+                <div class="colors" style="background-color:${colors.hex.value}">
+                </div>
+            `
+    }).join("")
 }
-
-getColorsBtn.addEventListener("click", () => getColors())
